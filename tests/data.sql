6f0dcc828b08cfee887752bd032ca9d7c4c5f9d3
SSG <gssenyonjo@gmail.com>
2023-04-22 09:23:08 +0300

    Setup conftest.py

--- tests/data.sql
+++ tests/data.sql
@@ -1,4 +1,6 @@
-INSERT INTO user(username, password) VALUES ('test', 'abc'), ('other', '123');
+INSERT INTO user(username, password)
+VALUES ('rob', 'pbkdf2:sha256:260000$57jvPacNqr38D4wJ$0f8027e32e55eb6890bc43a52ff52d22167a67e7b099d8363677e5705272d223');
+
 
 INSERT INTO post(title, body, author_id, created)
     VALUES
