1ca635c8f1b929666f968131e77f85bb0b6bbdd2
SSG <gssenyonjo@gmail.com>
2023-04-22 09:46:37 +0300

    Implemented tests test_db.py and test_factory.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,6 +5,7 @@ from . import db
 from . import auth
 from . import blog
 
+
 def create_app(test_config=None):
     # create and configure the app 
     app = Flask(__name__, instance_relative_config=True)
---
+++ tests/test_db.py
@@ -0,0 +1,32 @@
+import sqlite3
+from flaskr.db import get_db
+import pytest
+
+
+def test_get_close_db(app):
+    print('hey')
+    """
+    within an application context, get_db should return the same connection
+    each time it's called. After the context, the connection should be closed
+    """
+    with app.app_context():
+        db = get_db()
+        assert db is get_db()
+
+    with pytest.raises(sqlite3.ProgrammingError) as e:
+        db.execute('SELECT 1')
+
+    assert 'closed' in str(e.value)
+
+
+def test_init_db_command(runner, monkeypatch):
+    class Recorder(object):
+        called = False
+
+    def fake_init_db():
+        Recorder.called = True
+
+    monkeypatch.setattr('flaskr.db.init_db', fake_init_db)
+    result = runner.invoke(args=['init-db'])
+    assert 'Initialized' in result.output
+    assert Recorder.called
